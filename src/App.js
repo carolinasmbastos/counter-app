f22366ddc2a8c935639a0b4ee8481becb23a9add
carolinasmbastos <carolinasmbastos@gmail.com>
2020-02-26 21:53:53 -0800

    Create component read Next Counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
-import "./App.css";
-import RegisterUser from "./components/RegisterUser";
+import React from 'react';
+import './App.css';
+import RegisterUser from './components/RegisterUser';
+import NextCounter from './components/NextCounter'
 import axios from 'axios'
 
 class App extends React.Component {
   state = {
-    token: ""
+    token: '',
+    message: ''
   }
 
   getToken = (user) => {
     console.log(user);
+    axios.post('http://localhost:8080/auth/register', user)
+        .then(result => {
+            console.log(result.data.token)
+            this.setState({
+                token : result.data.token,
+                message : 'token saved'
+            })
+
+        })
+        .catch(error=> {
+            console.log(error)
+            this.setState({
+                message : error.message
+            })
+        })
 
   }
 
@@ -17,7 +34,11 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>UI to help test The Counter API</h1>
+        
         <RegisterUser getToken={this.getToken} />
+        <p>{this.state.message}</p>
+        <hr/>
+        <NextCounter token={this.state.token} />
       </div>
     );
   }
---
+++ src/components/NextCounter.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import axios from 'axios'
+
+class HelloWorldF extends React.Component {
+    state = {
+        counter : ''
+    }
+
+    getNextCounter = () => {
+        console.log(this.props.token)
+        axios.get('http://localhost:8080/api/next', {
+            headers: {
+              'Authorization': `Bearer ${this.props.token}`
+            }})
+            .then(result => {
+                console.log(result.data)
+                this.setState({
+                    counter : result.data.data.id
+                })
+                console.log(this.state.counter)
+            })
+            .catch(error=> {
+                console.log(error)
+                this.setState({
+                    message : error.message
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Next Counter Value: {this.state.counter}</h1>
+                <button onClick={this.getNextCounter} disabled={!this.props.token}>Get Next</button> 
+            </div>
+        )
+    }
+
+}
+
+export default HelloWorldF;
