6723a02cd09d740c3be642e4bc7346635c20fcba
carolinasmbastos <carolinasmbastos@gmail.com>
2020-02-26 21:17:39 -0800

    Create component Register User

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import RegisterUser from "./components/RegisterUser";
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    token: ""
+  }
+
+  getToken = (user) => {
+    console.log(user);
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>UI to help test The Counter API</h1>
+        <RegisterUser getToken={this.getToken} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/RegisterUser.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+class RegisterUser extends React.Component {
+
+    state = {
+        email : '',
+        password : ''
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name] : e.target.value})
+    }
+
+    submit = (e) => {
+        e.preventDefault();
+        let user = {
+            email: this.state.email, 
+            password: this.state.password 
+        }
+        this.props.getToken(user);
+        this.setState({password : ''})
+    }
+
+    render() {
+        return (        
+            <form onSubmit={this.submit}>
+                <input type="text" name="email" placeholder="enter the email" value={this.state.email} onChange={this.handleChange}/>
+                <input type="password" name="password" placeholder="enter the password" value={this.state.password} onChange={this.handleChange}/>
+                <input type="submit" value="submit"/>
+            </form>
+        )
+    }
+
+}
+
+export default RegisterUser;
